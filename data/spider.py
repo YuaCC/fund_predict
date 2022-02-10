d3a37ee08dc1a6801630f66a35c2af8e76a4ebd0
YuaCC <1095192336@qq.com>
2022-02-10 21:41:05 +0800

    README.md

--- data/spider.py
+++ data/spider.py
@@ -1,8 +1,4 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import datetime
 import time
 import os
 
